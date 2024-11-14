2a197cc27ae786bef15666a2ce06ecb2a9578404
KayuProg <KayuProg@gmail.com>
2024-11-15 01:36:55 +0900

    add title

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def main(page: ft.Page):
     page.horizontal_alignment=ft.CrossAxisAlignment.STRETCH
     page.title="Plan Bulletin"
     
-    calender_frame=frame.frame_class()
-    tasks_frame=frame.frame_class()
+    calender_frame=frame.frame_class("Calender")
+    tasks_frame=frame.frame_class("Tasks")
     
-    titels=ft.Row(controls=[calender_frame.col,tasks_frame.col],expand=True)#expandで縦画面サイズに合わせる
+    app=ft.Row(controls=[calender_frame.col,
+                            ft.VerticalDivider(color="white",width=1.5,thickness=1.5),#CalenderとTasksの区切り線
+                            tasks_frame.col]
+                  ,expand=True,spacing=0)#expandで縦画面サイズに合わせる,spacing=0でdividerの区切り消す．
     
 
-    page.add(titels)
+    page.add(app)
     page.update()
 
 ft.app(main)
