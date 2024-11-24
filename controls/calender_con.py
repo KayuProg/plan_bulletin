40588bbb05b599612360639d1269cda9dd80d6a5
KayuProg <KayuProg@gmail.com>
2024-11-24 14:15:47 +0900

    main bar large edit and change

--- controls/calender_con.py
+++ controls/calender_con.py
@@ -1,6 +1,37 @@
-from controls import frame#実行場所が一階層上だからfrom controls
+import flet as ft
 
-class calender_contents(frame.frame_class):#frame_class継承
-    def __init__(self,title):
-        super().__init__(title)#frame_classを継承してtitleを渡す．
+class calender_contents():
+    def __init__(self):
+        super().__init__()
         
+        # self.space= ft.Placeholder(color=ft.colors.random_color(),expand=True)#一時的な場所確保
+
+
+        self.main_bar=ft.Row(controls=[ft.Text("Today's schedule")
+                                    ],expand=True)
+
+        #ここのappendでcontrolsを追加してく感じで．
+        # self.col.controls.append(ft.Placeholder(color=ft.colors.random_color(),expand=True))
+        self.col.controls.append(self.main_bar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- controls/frame.py
+++ controls/frame.py
@@ -7,5 +7,7 @@ class frame_class(ft.Row):
         self.title_container=ft.Container(content=self.title,alignment=ft.alignment.center)#containerを使ってTitleを中央配置
         
         self.space= ft.Placeholder(color=ft.colors.random_color(),expand=True)#一時的な場所確保
-        self.col=ft.Column(controls=[self.title_container,ft.Divider(color="white",height=1.5,thickness=1.5)],expand=True)#expandで横画面の大きさに合わせる,height,thicknessでdividerの幅設定．
+        self.col=ft.Column(controls=[self.title_container,
+                                     ft.Divider(color="white",height=1.5,thickness=1.5),
+                                    ],expand=True)#expandで横画面の大きさに合わせる,height,thicknessでdividerの幅設定．
 
