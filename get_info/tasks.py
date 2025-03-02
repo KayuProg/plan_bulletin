afc9ae8c77f15027bd70d18f334b62b73463c8f2
KayuProg <KayuProg@gmail.com>
2025-03-02 15:27:43 +0900

    認証ができなかったが再度トークンを更新したら解決した．

--- get_info/tasks.py
+++ get_info/tasks.py
@@ -7,6 +7,11 @@ import os.path
 
 # 読み取り権限を指定
 SCOPES = ["https://www.googleapis.com/auth/tasks"]
+#うまく実行されないとき
+#google.auth.exceptions.RefreshError: ('invalid_scope: Bad Request', {'error': 'invalid_scope', 'error_description': 'Bad Request'})
+#というエラーが出たとき次のやつで実行するとよさそう．
+# SCOPES = ["https://www.googleapis.com/auth/tasks.readonly"]
+
 
 json_pass="./get_info/jsons/tasks_token.json"
 
@@ -21,32 +26,27 @@ def main():
         if creds and creds.expired and creds.refresh_token:#tokenの期限切れ確認
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("./jsons/parent.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("./get_info/jsons/parent.json", SCOPES)
             creds = flow.run_local_server(port=0)
         with open(json_pass, "w") as token:
             token.write(creds.to_json())
 
     try:
-        # Google Tasks APIのサービスを初期化
-        service = build("tasks", "v1", credentials=creds)#Google tasks APIを使うためのオブジェクト
+        service = build("tasks", "v1", credentials=creds)
 
+        # Call the Tasks API
+        results = service.tasklists().list(maxResults=10).execute()
+        items = results.get("items", [])
 
-
-        # タスクリストのIDを指定してタスクを取得
-        tasklist_id = "MTQzMjUyMTY0MTk3NDc0NDg1ODU6MDow"  # ここに取得したタスクリストIDを入れる
-        results = service.tasks().list(tasklist=tasklist_id).execute()
-        tasks = results.get("items", [])
-        # print(tasks)
-
-        if not tasks:
-            print("タスクリスト内にタスクはありません。")
+        if not items:
+            print("No task lists found.")
             return
 
-        print("タスク一覧:")
-        for task in tasks:
-            print(f"{task['title']} (Due: {task['due']})")
+        print("Task lists:")
+        for item in items:
+            print(f"{item['title']} ({item['id']})")
     except HttpError as err:
-        print(f"エラーが発生しました: {err}")
+        print(err)
 
 if __name__ == "__main__":
     main()
